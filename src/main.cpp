fb5543ca02c9c5b20fc12a34b783d01f3f1a16d9
TalusL <xiaoxiaochenjian@gmail.com>
2023-02-06 18:41:34 +0800

    add ZLMediaKit

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@ int main(){
     SipClient sipClient;
     sipClient.StartStack();
 
-    toolkit::TcpServer::Ptr rtspSrv = make_shared<toolkit::TcpServer>();
-    rtspSrv->start<mediakit::RtspSession>(554);
 
     this_thread::sleep_for(chrono::seconds(1000));
 }

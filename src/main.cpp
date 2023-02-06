a25a5e1eb9bcc568dc140ec329cdb241e87d9038
liangzhuohua <liangzhuohua@genew.com>
2023-02-06 18:39:26 +0800

    add ZLMediaKit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "SipClient.h"
+#include <Rtsp/RtspSession.h>
+#include <Network/TcpServer.h>
 int main(){
     SipClient sipClient;
     sipClient.StartStack();
+
+    toolkit::TcpServer::Ptr rtspSrv = make_shared<toolkit::TcpServer>();
+    rtspSrv->start<mediakit::RtspSession>(554);
 
     this_thread::sleep_for(chrono::seconds(1000));
 }

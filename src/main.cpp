c95057868fa22375c932aa97b431fce67bcc2af9
TalusL <xiaoxiaochenjian@gmail.com>
2023-02-07 10:40:29 +0800

    add ZL

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ int main(){
     SipClient sipClient;
     sipClient.StartStack();
 
+    toolkit::TcpServer::Ptr rtspSrv = make_shared<toolkit::TcpServer>();
+    rtspSrv->start<mediakit::RtspSession>(554);
 
     this_thread::sleep_for(chrono::seconds(1000));
 }
